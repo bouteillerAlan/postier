b0778a400aec08708b5031121799f4ad47692dc2
A2N <a2n.dev@pm.me>
2025-03-22 01:52:58 +0100

    ✨ add debug timer on card and a real stack of card

--- postier-app/src/App.tsx
+++ postier-app/src/App.tsx
@@ -1,9 +1,9 @@
 import {useEffect, useRef, useState} from 'react';
-import {Tabs, Container, Theme} from '@radix-ui/themes';
+import {Box, Container, Tabs, Theme} from '@radix-ui/themes';
 import RequestForm from './components/RequestForm';
 import ResponseViewer from './components/ResponseViewer';
 import RequestHistory from './components/RequestHistory';
-import {RequestData, Alert, PostierObjectWithMetrics} from './types/types.ts';
+import {Alert, PostierObjectWithMetrics, RequestData} from './types/types.ts';
 import {sendRequest} from './services/rust-http.ts';
 import {useRequestData} from './contexts/RequestContext.tsx';
 import {useHistoryData} from './contexts/HistoryContext.tsx';
@@ -19,7 +19,7 @@ function App() {
   const { historyData, setHistoryData } = useHistoryData();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [mainTabs, setMainTabs] = useState<string>('request');
-  const [alert, setAlert] = useState<Alert>({title: '', message: '', show: false});
+  const [alert, setAlert] = useState<Alert[]>([{title: '', message: '', show: false}]);
   const mainTabRef = useRef<HTMLDivElement>(null);
 
   /**
@@ -75,26 +75,18 @@ function App() {
   }, [mainTabs]);
 
   useEffect(() => {
-    setAlert({title: 'Debug', message: 'setting updated', show: setting.debug});
+    if (setting.debug) setAlert(prev => [...prev, {title: 'Debug', message: 'setting updated', show: setting.debug}]);
     writeContentInFile(JSON.stringify(setting), 'config.txt');
   }, [setting]);
 
   useEffect(() => {
-    setAlert({title: 'Debug', message: 'history updated', show: setting.debug});
+    if (setting.debug) setAlert(prev => [...prev, {title: 'Debug', message: 'history updated', show: setting.debug}]);
     writeContentInFile(JSON.stringify(historyData), 'history.txt');
   }, [historyData]);
 
   useEffect(() => {
-    setAlert({title: 'Debug', message: 'request updated', show: setting.debug});
+    if (setting.debug) setAlert(prev => [...prev, {title: 'Debug', message: 'request updated', show: setting.debug}]);
   }, [requestData]);
-
-  useEffect(() => {
-    if (alert.show) {
-      setTimeout(() => {
-        setAlert({title: '', message: '', show: false})
-      }, 8000)
-    }
-  }, [alert]);
 
   useEffect(() => {
     getContentFromFile('config.txt').then((value: string) => {
@@ -149,7 +141,11 @@ function App() {
           </Tabs.Root>
         </Container>
 
-        <AlertCard title={alert.title} message={alert.message} show={alert.show}/>
+        <Box style={{position: 'absolute', bottom: 10, right: 10, height: "fit-content", display: "flex", flexDirection: "column"}}>
+          {alert.length > 0 && alert.map((e, index) => (
+            <AlertCard key={`AlertCard${index}`} title={e.title} message={e.message} show={e.show}/>
+          ))}
+        </Box>
 
       </Theme>
     </ThemeProvider>
--- postier-app/src/components/AlertCard.tsx
+++ postier-app/src/components/AlertCard.tsx
@@ -1,15 +1,29 @@
 import {Box, Card, Text} from '@radix-ui/themes';
 import {BellIcon} from '@radix-ui/react-icons';
 import {Alert} from '../types/types.ts';
+import {useEffect, useRef, useState} from 'react';
+import {CircularProgress} from "./CircularProgress.tsx";
 
-// todo: latter use a stack of alert card
 export default function AlertCard(props: Alert) {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const [time, setTime] = useState<number>(100);
+
+  // todo: improve the animation of the svg
+  useEffect(() => {
+    if (time >= 0) {
+      setTimeout(() => {
+        setTime(time - 12.5); // 12.5 = 100% divided in 8s
+        if (cardRef.current && time <= 12.5) cardRef.current.remove();
+      }, 1000)
+    }
+  }, [time]);
+
   return (
     props.show &&
-    <Box maxWidth='350px' minWidth='150px' style={{position: 'absolute', bottom: 10, right: 10}}>
-      <Card variant='surface' style={{ backgroundColor: 'var(--accent-1)'}}>
-        <Text as='div' size='2' weight='bold'>
-          <BellIcon/> {props.title}
+    <Box ref={cardRef} maxWidth='350px' minWidth='150px' mt='10px'>
+      <Card variant='surface' style={{backgroundColor: 'var(--accent-1)'}}>
+        <Text as='div' size='2' weight='bold' style={{display: 'flex', justifyContent: 'space-between'}}>
+          <span><BellIcon/> {props.title}</span> <CircularProgress value={time} style={{height: 15, color: 'var(--accent-9)'}}/>
         </Text>
         <Text as='div' color='gray' size='2'>
           {props.message}
