ac066acee8d012a8ff4d9c5699dc7f0abfd523e5
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-04-19 22:59:19 +0200

    💄 add some style in the history viewer

--- postier-app/src/components/response/Pretty.tsx
+++ postier-app/src/components/response/Pretty.tsx
@@ -1,16 +1,27 @@
-import {Card} from '@radix-ui/themes';
-import {PrismTheme} from 'prism-react-renderer';
+import {Card, ScrollArea} from '@radix-ui/themes';
+import {PrismTheme, themes} from 'prism-react-renderer';
 import HighlightCode from "../codeHighlighting/HighlightCode.tsx";
+import {UserSetting} from "../../types/types.ts";
 
-export default function PrettyResponse (props: {data: string, viewHeight: number, contentType: string, codeTheme: PrismTheme | undefined}) {
+interface PrettyResponseProps {
+  data: string;
+  viewHeight: number;
+  contentType: string;
+  codeTheme: PrismTheme | undefined;
+  userConfig: UserSetting;
+}
+
+export default function PrettyResponse (props: PrettyResponseProps) {
   return (
     <Card style={{padding: 0, height: props.viewHeight}}>
-      <HighlightCode
-        codeTheme={props.codeTheme}
-        content={props.data}
-        contentType={props.contentType}
-        mainDivStyle={{padding: 10, height: props.viewHeight - 20}}
-      />
+      <ScrollArea style={{padding: 0, backgroundColor: themes[props.userConfig.codeTheme].plain.backgroundColor}}>
+        <HighlightCode
+          codeTheme={props.codeTheme}
+          content={props.data}
+          contentType={props.contentType}
+          mainDivStyle={{margin: 10, height: props.viewHeight-20}}
+        />
+      </ScrollArea>
     </Card>
   );
 }
